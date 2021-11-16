70e575dbbf74f6677f2085c8e92d58af08303121
Jose L. Velez <joselvelez@protonmail.com>
2021-11-15 22:43:42 -0600

    minor fixes

--- src/components/PunchcardMintBtn.js
+++ src/components/PunchcardMintBtn.js
@@ -3,6 +3,7 @@ import { WalletContext } from "../context/WalletContext";
 import { defaultPunchcardAmount } from "../constants/contractConstants";
 import { WrongChain } from "./WrongChain";
 import { mintPunchcard } from "../contracts/contractAPI";
+import loadingGif from '../../public/images/loadingIndicator.gif';
 
 export const PunchcardMintBtn = ({ currentPrice, setIsMinting, isMinting }) => {
     const walletContext = useContext(WalletContext);
@@ -23,7 +24,7 @@ export const PunchcardMintBtn = ({ currentPrice, setIsMinting, isMinting }) => {
                 walletContext.state.currentChain === walletContext.state.configuredChain ?
                     <>
                         {
-                            isMinting ? <img src="./../images/loadingindicator.gif" alt="loading" className="sm:h-32 md:h-48" /> : 
+                            isMinting ? <img src="loadingGif" alt="loading" className="sm:h-32 md:h-48" /> : 
                             <>
                                 <p className="text-lg leading-6 font-medium text-gray-900">Mint a Punchcard*</p>
                                 <p className="text-lg leading-6 font-medium text-gray-900">Pay once, own it forever</p>
