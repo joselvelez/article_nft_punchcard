6af2d6e2fd81cd9a6f5569b172f11b39cc24af3e
Jose L. Velez <joselvelez@protonmail.com>
2021-11-15 23:45:42 -0600

    fix install mm link

--- src/components/InstallWalletBtn.js
+++ src/components/InstallWalletBtn.js
@@ -1,14 +1,12 @@
 export const InstallWalletBtn = () => {
     return (
         <>
-            <button
-                type="button"
-                onClick={window.open('https://metamask.io/')}
+            <a href="https://metamask.io/" rel="noreferrer" target="_blank"
                 className="inline-flex items-center mx-1 mb-1 px-2 py-1 border border-transparent text-xs font-medium rounded 
                 shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             >
                 Get MetaMask
-            </button>
+            </a>
         </>
     )
 }
